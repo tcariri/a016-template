9729c3680c6bcd530f6a73e56647317c02a1819c
tcariri <thais.cariri2016@gmail.com>
2022-09-15 15:48:15 -0400

    começo da aula

--- script.js
+++ script.js
@@ -1,37 +1,59 @@
-const filmes = [
-  {
-    titulo: 'O Auto da Compadecida',
-    ano: 2000,
-    diretor: 'Guel Arraes',
-    elenco: [
-      'Selton Mello',
-      'Mateus Nachtergaele',
-      'Marco Nanini',
-      'Fernanda Montenegro',
-    ],
-  },
-  {
-    titulo: 'Carandiru',
-    ano: 2001,
-    diretor: 'Hector Babenco',
-    elenco: [
-      'Wagner Moura',
-      'José Carlos Vasconcelos',
-      'Ailton Graça',
-      'Caio Blat',
-    ],
-  },
-  {
-    titulo: 'Aquarius',
-    ano: 2012,
-    diretor: 'Kléber Mendonça Filho',
-    elenco: [
-      'Sônia Braga',
-      'Humberto Carrão',
-      'Maeve Jinkings',
-      'Bárbara Colen',
-    ],
-  },
-]
+// const filmes = [
+//   {
+//     titulo: 'O Auto da Compadecida',
+//     ano: 2000,
+//     diretor: 'Guel Arraes',
+//     elenco: [
+//       'Selton Mello',
+//       'Mateus Nachtergaele',
+//       'Marco Nanini',
+//       'Fernanda Montenegro',
+//     ],
+//   },
+//   {
+//     titulo: 'Carandiru',
+//     ano: 2001,
+//     diretor: 'Hector Babenco',
+//     elenco: [
+//       'Wagner Moura',
+//       'José Carlos Vasconcelos',
+//       'Ailton Graça',
+//       'Caio Blat',
+//     ],
+//   },
+//   {
+//     titulo: 'Aquarius',
+//     ano: 2012,
+//     diretor: 'Kléber Mendonça Filho',
+//     elenco: [
+//       'Sônia Braga',
+//       'Humberto Carrão',
+//       'Maeve Jinkings',
+//       'Bárbara Colen',
+//     ],
+//   },
+
+// ]
+
+
 
 // escreva seu código abaixo 👇🏻
+
+// exercicio 1
+
+
+const resultado1 = [2,4,6,8]
+const resultado2 = [1,3,5,7]
+const resultado3 = [0,8,10,12]
+const resultado4 = [9,11,13,15]
+
+const megSena = [resultado1,resultado2,resultado3,resultado4]
+
+console.log(megSena)
+
+if (megSena.length === 4){
+   
+}else{
+  console.log("é nececssario alterar o número de itens ")
+}
+
